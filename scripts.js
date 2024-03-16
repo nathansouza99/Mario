2f4691ee61bed805ca4f6b811c68130e8aff234c
nathansouza99 <nathan.souza6131@gmail.com>
2024-03-16 12:48:30 -0300

    Projeto mario-bros

---
+++ scripts.js
@@ -0,0 +1,17 @@
+const form = document.querySelector('.formulario-fale-conosco')
+const background = document.querySelector('.mascara-form')
+
+function showForm() {
+    form.style.left = '50%'
+    form.style.transform = 'translateX(-50%)'
+    background.style.visibility = 'visible'
+}
+
+function hideForm() {
+    form.style.left = '-330px'
+    form.style.transform = 'translateX(0)'
+    background.style.visibility = 'hidden'
+}
+
+
+
